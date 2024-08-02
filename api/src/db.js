0ff530404d194d0d9261f631162b19034e0698da
Maira Zamer <mairazamer@gmail.com>
2024-08-02 13:11:48 -0300

    db

--- api/src/db.js
+++ api/src/db.js
@@ -2,38 +2,68 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const definePokemon = require('./models/Pokemon');
-const defineType = require('./models/Type');
 
 const sequelize = new Sequelize({
-   database: process.env.DB_DATABASE,
-   username: process.env.DB_USERNAME,
-   password: process.env.DB_PASSWORD,
-   host: process.env.DB_HOST,
-   port: process.env.DB_PORT,
-   dialect: 'postgres',
-   dialectOptions: {
-     ssl: process.env.DB_SSL === 'true' ? {
-       require: true,
-       rejectUnauthorized: false // Ajusta esto según tu configuración de certificados SSL
-     } : false
-   },
-   logging: false // Opcional: desactivar el registro de consultas
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.DB_SSL === 'true' ? {
+      require: true,
+      rejectUnauthorized: false // Ajusta esto según tu configuración de certificados SSL
+    } : false
+  },
+  logging: false // Opcional: desactivar el registro de consultas
 });
 
-// Define los modelos
-definePokemon(sequelize);
-defineType(sequelize);
+// Verificar conexión
+sequelize.authenticate()
+   .then(() => {
+      console.log('Connection has been established successfully.');
+   })
+   .catch(err => {
+      console.error('Unable to connect to the database:', err);
+   });
 
-// Obtén los modelos después de definirlos
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, '/models'))
+   .filter(
+      (file) =>
+         file.indexOf('.') !== 0 &&
+         file !== basename &&
+         file.slice(-3) === '.js'
+   )
+   .forEach((file) => {
+      modelDefiners.push(require(path.join(__dirname, '/models', file)));
+   });
+
+// Injectamos la conexion (sequelize) a todos los modelos
+modelDefiners.forEach((model) => model(sequelize));
+// Capitalizamos los nombres de los modelos ie: product => Product
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+   entry[0][0].toUpperCase() + entry[0].slice(1),
+   entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+// En sequelize.models están todos los modelos importados como propiedades
+// Para relacionarlos hacemos un destructuring
 const { Pokemon, Type } = sequelize.models;
 
-// Define las relaciones
+// Aca vendrian las relaciones
 Pokemon.belongsToMany(Type, { through: "pokemon_type" });
 Type.belongsToMany(Pokemon, { through: "pokemon_type" });
+// Product.hasMany(Reviews);
 
 module.exports = {
-   conn: sequelize,
-   Pokemon,
-   Type,
+   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+   conn: sequelize, // para importar la conexión { conn } = require('./db.js');
 };
